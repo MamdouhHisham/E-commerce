c2c60bfcc6e3eab734aa358fb1e8d387ab628e3e
MamdouhHisham <mamdouhhisham@std.mans.edu.eg>
2025-11-28 22:11:30 +0200

    edit validation and add discovery service

--- product-service/src/main/java/com/example/productservice/client/InventoryClient.java
+++ product-service/src/main/java/com/example/productservice/client/InventoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "inventory-service", url = "${inventory-service.url}")
+@FeignClient(name = "INVENTORYSERVICE")
 public interface InventoryClient {
 
     @PostMapping("/api/inventory")
--- product-service/src/main/java/com/example/productservice/dto/ProductDTO.java
+++ product-service/src/main/java/com/example/productservice/dto/ProductDTO.java
@@ -12,15 +12,14 @@ public class ProductDTO {
     private String description;
 
     @Positive(message = "Product price must be positive") @Min(value = 1, message = "Product price must be at least 1")
-    @NotEmpty
     private double price;
 
     @NotBlank(message = "Product SKU code cannot be blank") @NotEmpty
     private String skuCode;
 
-    @NotNull(message = "Category ID cannot be null") @NotEmpty @Min(value = 1)
+    @NotNull(message = "Category ID cannot be null") @Min(value = 1)
     private Long categoryId;
 
-    @NotNull(message = "Quantity cannot be null") @Min(value = 1) @NotEmpty
+    @NotNull(message = "Quantity cannot be null") @Min(value = 1)
     private Long quantity;
 }
