c4bd1b8e0381ff89803d10676d06370771b8a54a
MamdouhHisham <mamdouhhisham@std.mans.edu.eg>
2025-11-29 15:08:22 +0200

    init order service with model

---
+++ order-service/src/main/java/com/example/orderservice/model/Order.java
@@ -0,0 +1,40 @@
+package com.example.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity(name = "order")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_number", nullable = false, unique = true)
+    private String orderNumber;
+
+    @Column(name = "total_price")
+    private double totalPrice;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    @PrePersist
+    private void onCreate(){
+        orderDate = LocalDateTime.now();
+        status = OrderStatus.PENDING;
+    }
+}
---
+++ order-service/src/main/java/com/example/orderservice/model/OrderItem.java
@@ -0,0 +1,28 @@
+package com.example.orderservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String skuCode;
+
+    private Integer quantity;
+
+    private Double price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+}
